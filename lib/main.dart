e184d046abf6d255cac7781f2a3b5d11edc0ba3c
DeleMike <akindelemichael65@gmail.com>
2020-12-16 13:06:59 +0100

    add app theme

--- lib/main.dart
+++ lib/main.dart
@@ -10,17 +10,34 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.indigo,
+        accentColor: Color(0xff3f51b5),
+        canvasColor: Colors.indigo[50],
+        fontFamily: 'Ubuntu',
+        textTheme: ThemeData.light().textTheme.copyWith(
+          bodyText2: TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+          bodyText1: TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+          headline6: TextStyle(
+                fontSize: 12,
+                fontFamily: 'Ubuntu',
+                fontWeight: FontWeight.bold,
+              ),
+        ) ,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: Scaffold(
         appBar: AppBar(
-          title: Text('trivia'),
+          title: Text('Trivia'),
         ),
         body: Center(
-            child: Text('Hello world!'),
+            child: Text('Hello world!', style:  Theme.of(context).textTheme.headline6,),
           ),
       ),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
