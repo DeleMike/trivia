527a2e046ffd8bcec23aceed7cb2a092b4a497a1
DeleMike <akindelemichael65@gmail.com>
2020-12-17 20:54:13 +0100

    add text banner to auth screen

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import '../widgets/image_banner.dart';
 import '../widgets/auth_form.dart';
+import '../widgets/text_banner.dart';
 
 class AuthScreen extends StatelessWidget {
   @override
@@ -9,36 +10,38 @@ class AuthScreen extends StatelessWidget {
     final deviceSize = MediaQuery.of(context).size;
     return Scaffold(
       body: Stack(
-        children: [
-          Container(
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                colors: [Colors.indigo[300], Colors.indigo[900]],
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-                stops: [0, 0.7],
-              ),
-            ),
-          ),
-          SingleChildScrollView(
-            child: Container(
-              width: deviceSize.width,
-              height: deviceSize.height,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Flexible(child: const ImageBanner()),
-                  Flexible(
-                    flex: deviceSize.width > 600 ? 2 : 1,
-                    child: AuthForm(),
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ],
+          children: [
+            Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [Colors.indigo[300], Colors.indigo[900]],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          stops: [0, 0.7],
+        ),
       ),
+            ),
+            SingleChildScrollView(
+      child: Container(
+        width: deviceSize.width,
+        height: deviceSize.height,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(height: deviceSize.height * 0.07),
+            Flexible(child: const ImageBanner()),
+            Flexible(child: const TextBanner()),
+            Flexible(
+              flex: deviceSize.width > 600 ? 3 : 2,
+              child: AuthForm(),
+            ),
+          ],
+        ),
+      ),
+            ),
+          ],
+        ),
     );
   }
 }
---
+++ lib/widgets/text_banner.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class TextBanner extends StatelessWidget {
+  const TextBanner();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(bottom: 10.0),
+      child: Stack(
+        children: [
+          //stroked text
+          Text(
+            'Trivia!',
+            style: TextStyle(
+              fontSize: 40,
+              foreground: Paint()
+                ..style = PaintingStyle.stroke
+                ..strokeWidth = 2.2
+                ..color = Colors.white,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
