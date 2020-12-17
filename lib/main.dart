b9f71be09b6913b3376d4b9537fb90b66278b8c7
DeleMike <akindelemichael65@gmail.com>
2020-12-17 23:28:16 +0100

    add provider package

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import './screens/auth_screen.dart';
+import './screens/welcome_screen.dart';
+import './helpers/user_pref.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,45 +12,44 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.indigo,
-        accentColor: Color(0xff3f51b5),
-        canvasColor: Colors.indigo[50],
-        fontFamily: 'Ubuntu',
-        accentColorBrightness: Brightness.dark,
-        buttonTheme: ButtonTheme.of(context).copyWith(
-          buttonColor: Colors.indigo,
-          textTheme: ButtonTextTheme.primary,
-          shape:
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+    return ChangeNotifierProvider(
+      create: (ctx) => UserPref(),
+      child: Consumer<UserPref>(
+        builder: (_, userPref, __) => MaterialApp(
+          title: 'Flutter Demo',
+          theme: ThemeData(
+            primarySwatch: Colors.indigo,
+            accentColor: Color(0xff3f51b5),
+            canvasColor: Colors.indigo[50],
+            fontFamily: 'Ubuntu',
+            accentColorBrightness: Brightness.dark,
+            buttonTheme: ButtonTheme.of(context).copyWith(
+              buttonColor: Colors.indigo,
+              textTheme: ButtonTextTheme.primary,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20)),
+            ),
+            textTheme: ThemeData.light().textTheme.copyWith(
+                  bodyText2: TextStyle(
+                    color: Color.fromRGBO(20, 51, 51, 1),
+                  ),
+                  bodyText1: TextStyle(
+                    color: Color.fromRGBO(20, 51, 51, 1),
+                  ),
+                  headline6: TextStyle(
+                    fontSize: 12,
+                    fontFamily: 'Ubuntu',
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+            visualDensity: VisualDensity.adaptivePlatformDensity,
+          ),
+          home: !userPref.isLogin
+              ? AuthScreen()
+              : WelcomeScreen(),
+          debugShowCheckedModeBanner: false,
         ),
-        textTheme: ThemeData.light().textTheme.copyWith(
-          bodyText2: TextStyle(
-                color: Color.fromRGBO(20, 51, 51, 1),
-              ),
-          bodyText1: TextStyle(
-                color: Color.fromRGBO(20, 51, 51, 1),
-              ),
-          headline6: TextStyle(
-                fontSize: 12,
-                fontFamily: 'Ubuntu',
-                fontWeight: FontWeight.bold,
-              ),
-        ) ,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: AuthScreen(),
-      // home: Scaffold(
-      //   appBar: AppBar(
-      //     title: Text('Trivia'),
-      //   ),
-      //   body: Center(
-      //       child: Text('Hello world!', style:  Theme.of(context).textTheme.headline6,),
-      //     ),
-      // ),
-      debugShowCheckedModeBanner: false,
     );
   }
 }
