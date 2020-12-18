f62c66428b178798f8a2a694585d765c877f96ac
DeleMike <akindelemichael65@gmail.com>
2020-12-18 13:13:35 +0100

    finish welcome screen

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -9,6 +9,7 @@ import '../widgets/nav_drawer.dart';
 class WelcomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final deviceSize = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
         title: Text('Trivia'),
@@ -21,12 +22,66 @@ class WelcomeScreen extends StatelessWidget {
             }
             return Consumer<UserPref>(
               builder: (_, userPref, __) => NavDrawer(
-                  username: userPref.userData['username'],
-                  fileImage: File(userPref.userData['imagepath'])),
+                username: userPref.userData['username'],
+                fileImage: File(
+                  userPref.userData['imagepath'],
+                ),
+              ),
             );
           }),
-      body: Center(
-        child: Text('Welcome screen'),
+      body: SingleChildScrollView(
+        child: Container(
+          width: deviceSize.width,
+          height: deviceSize.height,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Flexible(
+                child: Container(
+                  width: deviceSize.width * 0.2,
+                  child: Image.asset('assets/images/app_icon.png'),
+                ),
+              ),
+              SizedBox(height: 25),
+              Consumer<UserPref>(
+                builder: (_, userPref, __) => Expanded(
+                  child: Text(
+                    'Welcome, ${userPref.userData['username']}',
+                    style: TextStyle(
+                      fontSize: 40,
+                      foreground: Paint()
+                        ..style = PaintingStyle.stroke
+                        ..strokeWidth = 2.5
+                        ..color = Theme.of(context).primaryColor,
+                    ),
+                  ),
+                ),
+              ),
+              Flexible(
+                child: Text(
+                  'ready to test your knowledge ?',
+                ),
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      margin: const EdgeInsets.all(16.0),
+                      child: RaisedButton(
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 15),
+                          child: Text('Go'),
+                        ),
+                        onPressed: () {},
+                      ),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
