8a845ec39a0a946303e1761921e7eb17b74e141a
DeleMike <akindelemichael65@gmail.com>
2020-12-18 12:17:45 +0100

    finish nav drawer

--- lib/widgets/nav_drawer.dart
+++ lib/widgets/nav_drawer.dart
@@ -2,65 +2,57 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 
-import '../helpers/user_pref.dart';
-
-class NavDrawer extends StatefulWidget {
+class NavDrawer extends StatelessWidget {
   final String username;
   final File fileImage;
 
   NavDrawer({this.username, this.fileImage});
 
   @override
-  _NavDrawerState createState() => _NavDrawerState();
-}
-
-class _NavDrawerState extends State<NavDrawer> {
-  String _username = '';
-  File _imageFile;
-
-  Future _getUserData() async {
-    final userPref = UserPref();
-    final userData = await userPref.fetchData();
-    _username = userData['username'];
-    final imageFilePath = userData['imagepath'];
-    print('NavDrawer: username = $_username');
-    print('NavDrawer: imageFilePath = $imageFilePath');
-    _imageFile = File(imageFilePath);
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    _getUserData();
-  }
-
-  @override
   Widget build(BuildContext context) {
     final drawerHeader = UserAccountsDrawerHeader(
-      accountName: Text('$_username'),
+      decoration: BoxDecoration(color: Theme.of(context).primaryColor),
+      accountName: Text('$username'),
       accountEmail: null,
       currentAccountPicture: CircleAvatar(
         radius: 40,
-        backgroundImage: _imageFile != null
-            ? FileImage(_imageFile)
+        backgroundImage: fileImage != null
+            ? FileImage(fileImage)
             : AssetImage('assets/images/app_icon.png'),
       ),
     );
 
     final drawerItems = ListView(
       children: [
-        drawerHeader,
-        InkWell(
-          child: ListTile(
-            title: Text('History'),
-          ),
-          onTap: () {},
+        Container(
+          width: double.infinity,
+          child: drawerHeader,
         ),
         InkWell(
           child: ListTile(
-            title: Text('Settings'),
+            title: Text(
+              'History',
+              style: TextStyle(
+                fontStyle: FontStyle.normal,
+              ),
+            ),
           ),
-          onTap: () {},
+          onTap: () {
+            Navigator.of(context).pop();
+          },
+        ),
+        InkWell(
+          child: ListTile(
+            title: Text(
+              'Settings',
+              style: TextStyle(
+                fontStyle: FontStyle.normal,
+              ),
+            ),
+          ),
+          onTap: () {
+            Navigator.of(context).pop();
+          },
         ),
       ],
     );
