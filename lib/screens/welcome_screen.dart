943e66fb763e733b7508014546fc288e0b896697
DeleMike <akindelemichael65@gmail.com>
2020-12-18 12:33:41 +0100

    add leading icon

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -10,7 +10,34 @@ class WelcomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Trivia')),
+      appBar: AppBar(
+        title: Text('Trivia'),
+        //leading: Image.file(file),
+        leading: FutureBuilder(
+            future: Provider.of<UserPref>(context, listen: false).fetchData(),
+            builder: (ctx, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return Container();
+              }
+              return GestureDetector(
+                onTap: () {
+                  Scaffold.of(ctx).openDrawer();
+                },
+                child: Consumer<UserPref>(
+                  builder: (_, userPref, __) => Padding(
+                    padding: const EdgeInsets.only(left: 12.0),
+                    child: CircleAvatar(
+                      radius: 6,
+                      backgroundImage: FileImage(
+                          File(userPref.userData['imagepath']) == null
+                              ? null
+                              : File(userPref.userData['imagepath'])),
+                    ),
+                  ),
+                ),
+              );
+            }),
+      ),
       drawer: FutureBuilder(
           future: Provider.of<UserPref>(context, listen: false).fetchData(),
           builder: (ctx, snapshot) {
@@ -18,8 +45,9 @@ class WelcomeScreen extends StatelessWidget {
               return Container();
             }
             return Consumer<UserPref>(
-              builder: (_, userPref, __) => 
-                NavDrawer(username: userPref.userData['username'], fileImage: File(userPref.userData['imagepath'])),
+              builder: (_, userPref, __) => NavDrawer(
+                  username: userPref.userData['username'],
+                  fileImage: File(userPref.userData['imagepath'])),
             );
           }),
       body: Center(
