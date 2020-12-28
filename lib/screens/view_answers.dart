dc643d1fe791a591c02df4ed957bb5cbed645cb8
DeleMike <akindelemichael65@gmail.com>
2020-12-28 15:19:03 +0100

    view answers page finish

--- lib/screens/view_answers.dart
+++ lib/screens/view_answers.dart
@@ -2,50 +2,48 @@ import 'package:flutter/material.dart';
 
 class ViewAnswers extends StatelessWidget {
   static const routeName = '/view-answers';
+  final widgets = <Widget>[
+    Text('Hello - 1'),
+    Text('Hello - 2'),
+    Text('Hello - 3'),
+    Text('Hello - 4'),
+  ];
   @override
   Widget build(BuildContext context) {
-    final tabs = ModalRoute.of(context).settings.arguments as List<Widget>;
+    final tabs = ModalRoute.of(context).settings.arguments as Map<String, List<String>>;
+    final questions = tabs['question'];
+    final answers = tabs['answer'];
+    print('Tabs: $tabs');
+    print('Tabs Length: ${tabs.length}');
     return Scaffold(
       appBar: AppBar(title: Text('View Correct Answers')),
-      body: DefaultTabController(
-        length: tabs.length,
-        child: Builder(
-          builder: (ctx) {
-            return Container(
-              padding: const EdgeInsets.all(8.0),
-              child: SingleChildScrollView(
+      body:
+          PageView.builder(
+            itemCount: tabs.length,
+            itemBuilder: (ctx, i) {
+              return Container(
+                 padding: const EdgeInsets.all(8.0),
+                 margin: const EdgeInsets.all(8.0),
                 child: Column(
-                  children: [
-                    TabPageSelector(),
-                    TabBarView(children: tabs),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        RaisedButton(
-                          child: Text('Finish'),
-                          onPressed: () {
-                            print('TabView: Finish');
-                          },
-                        ),
-                        RaisedButton(
-                          child: Text('Skip'),
-                          onPressed: () {
-                            print('TabView: Skip to end');
-                            final controller = DefaultTabController.of(context);
-                            if(!controller.indexIsChanging){
-                              controller.animateTo(tabs.length - 1);
-                            }
-                          },
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-            );
-          },
-        ),
-      ),
+                    children: [
+                      SizedBox(height:10),
+                      Text('Question ${i+1}'),
+                      SizedBox(height:10),
+                      Container(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(questions[i], style: Theme.of(context).textTheme.headline6)),
+                        SizedBox(height:10),
+                        Divider(),
+                        SizedBox(height:10),
+                       Container(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(answers[i], style: Theme.of(context).textTheme.headline6)),
+                      
+                    ],
+                  ),
+              );
+            },
+          ),
     );
   }
 }
