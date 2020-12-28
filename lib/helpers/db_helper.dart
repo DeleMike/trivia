a610fc2304ce7b7a4c04dc525c1f14c6393c6060
DeleMike <akindelemichael65@gmail.com>
2020-12-28 19:45:19 +0100

    create db helper class

---
+++ lib/helpers/db_helper.dart
@@ -0,0 +1,34 @@
+import 'package:sqflite/sqflite.dart' as sql;
+import 'package:path/path.dart' as path;
+
+///Opens SQLite functions tothe app
+class DbHelper {
+  ///create DB
+  static Future<sql.Database> database() async {
+    final dbPath = await sql.getDatabasesPath();
+    return sql.openDatabase(
+      path.join(dbPath, 'trivia.db'),
+      onCreate: (db, version) {
+        return db.execute(
+            'CREATE TABLE history(id INTEGER PRIMARY KEY AUTOINCREMENT, quizName TEXT, quizDifficulty TEXT, timeTaken TEXT)');
+      },
+      version: 1,
+    );
+  }
+
+  ///insert data into DB
+  static Future<void> insert(String table, Map<String, Object> data) async {
+    final sqlDb = await DbHelper.database();
+    sqlDb.insert(
+      table,
+      data,
+      conflictAlgorithm: sql.ConflictAlgorithm.replace,
+    );
+  }
+
+  ///fetch data from db
+  static Future<List<Map<String, dynamic>>> getData(String table) async {
+    final sqlDb = await DbHelper.database();
+    return sqlDb.query(table);
+  }
+}
