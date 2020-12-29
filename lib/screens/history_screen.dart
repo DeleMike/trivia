986c7c9fbabf7d735bc8608f2b142a4d53d1db39
DeleMike <akindelemichael65@gmail.com>
2020-12-29 10:08:40 +0100

    configure list UI

--- lib/screens/history_screen.dart
+++ lib/screens/history_screen.dart
@@ -21,40 +21,49 @@ class HistoryScreen extends StatelessWidget {
                 child: Center(child: Text('You got no history. Take a quiz.')),
                 builder: (ctx, history, ch) => history.items.length == 0
                     ? ch
-                    : ListView.builder(
-                        itemCount: history.items.length,
-                        itemBuilder: (ctx, i) {
-                          int diffStrength = 100;
-                          if (history.items[i].difficulty == 'easy') {
-                            diffStrength = 300;
-                          } else if (history.items[i].difficulty == 'medium') {
-                            diffStrength = 600;
-                          } else {
-                            diffStrength = 900;
-                          }
+                    : Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: ListView.builder(
+                          itemCount: history.items.length,
+                          itemBuilder: (ctx, i) {
+                            int diffStrength = 100;
+                            if (history.items[i].difficulty == 'easy') {
+                              diffStrength = 300;
+                            } else if (history.items[i].difficulty == 'medium') {
+                              diffStrength = 600;
+                            } else {
+                              diffStrength = 900;
+                            }
 
-                          return Container(
-                            margin: const EdgeInsets.only(bottom: 8.0, left: 8.0, right: 8.0,),
-                            child: Card(
-                              elevation: 4.0,
-                              child: InkWell(
-                                onTap: () {},
-                                child: ListTile(
-                                  leading: CircleAvatar(
-                                    backgroundColor:
-                                        Colors.indigo[diffStrength],
+                            return Container(
+                              margin: const EdgeInsets.only(bottom: 8.0, left: 8.0, right: 8.0,),
+                              child: Card(
+                                elevation: 4.0,
+                                child: InkWell(
+                                  onTap: () {},
+                                  child: ListTile(
+                                    leading: CircleAvatar(
+                                      backgroundColor:
+                                          Colors.indigo[diffStrength],
+                                    ),
+                                    title: Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Text('${history.items[i].name}', style: Theme.of(context).textTheme.headline1),
+                                    ),
+                                    subtitle: Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Text(
+                                        'Score: ${history.items[i].score}',
+                                      ),
+                                    ),
+                                    trailing: Text(
+                                        'Date: ${history.items[i].dateTaken}'),
                                   ),
-                                  title: Text('Name: ${history.items[i].name}'),
-                                  subtitle: Text(
-                                    'Score: ${history.items[i].score}',
-                                  ),
-                                  trailing: Text(
-                                      'Time taken: ${history.items[i].dateTaken}'),
                                 ),
                               ),
-                            ),
-                          );
-                        }),
+                            );
+                          }),
+                    ),
               ),
       ),
     );
