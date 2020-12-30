41f6b279aa7841a58c678c34b9497a50205997ef
DeleMike <akindelemichael65@gmail.com>
2020-12-30 14:03:42 +0100

    affect app state

--- lib/screens/settings.dart
+++ lib/screens/settings.dart
@@ -1,9 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:settings_ui/settings_ui.dart';
+import 'package:provider/provider.dart';
+import 'package:trivia/helpers/dark_theme_provider.dart';
 
-class Settings extends StatelessWidget {
+class Settings extends StatefulWidget {
+  static const routeName = 'settings';
+
+  @override
+  _SettingsState createState() => _SettingsState();
+}
+
+class _SettingsState extends State<Settings> {
+  var _value = false;
+
   @override
   Widget build(BuildContext context) {
-    return Container(
+    final themeChange = Provider.of<DarkThemeProvider>(context);
+    return Scaffold(
+      appBar: AppBar(title: Text('Settings')),
+      body: Container(
+        padding: const EdgeInsets.only(top: 10.0),
+        child: SettingsList(
+          sections: [
+            SettingsSection(
+              title: 'Preferences',
+              tiles: [
+                SettingsTile.switchTile(
+                  title: 'Dark Mode',
+                  leading: Icon(_value ? Icons.bedtime : Icons.bedtime_outlined, color: Colors.indigo[600],),
+                  switchValue: themeChange.darkTheme,
+                  onToggle: (bool val) {
+                    themeChange.darkTheme = val;
+                    print('Settings: Dark Mode = $val');
+                  },
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
       
     );
   }
