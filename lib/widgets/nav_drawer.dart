5690aa949a5dcd6e65ede5c6bac9ec09110c62af
DeleMike <akindelemichael65@gmail.com>
2021-01-02 13:17:42 +0100

    create the about dialog

--- lib/widgets/nav_drawer.dart
+++ lib/widgets/nav_drawer.dart
@@ -2,8 +2,8 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:trivia/helpers/dark_theme_provider.dart';
 
+import '../helpers/dark_theme_provider.dart';
 import '../screens/history_screen.dart';
 import '../screens/settings.dart';
 
@@ -37,6 +37,12 @@ class NavDrawer extends StatelessWidget {
         ),
         InkWell(
           child: ListTile(
+            leading: Icon(
+              Icons.note,
+              color: themeProvider.darkTheme
+                  ? Theme.of(context).buttonTheme.colorScheme.surface
+                  : Theme.of(context).primaryColor,
+            ),
             title: Text(
               'History',
               style: TextStyle(
@@ -53,6 +59,12 @@ class NavDrawer extends StatelessWidget {
         ),
         InkWell(
           child: ListTile(
+            leading: Icon(
+              Icons.settings,
+              color: themeProvider.darkTheme
+                  ? Theme.of(context).buttonTheme.colorScheme.surface
+                  : Theme.of(context).primaryColor,
+            ),
             title: Text(
               'Settings',
               style: TextStyle(
@@ -66,6 +78,41 @@ class NavDrawer extends StatelessWidget {
           onTap: () {
             Navigator.of(context).popAndPushNamed(Settings.routeName);
           },
+        ),
+        InkWell(
+          child: AboutListTile(
+            icon: Icon(
+              Icons.info_outline_rounded,
+              color: themeProvider.darkTheme
+                  ? Theme.of(context).buttonTheme.colorScheme.surface
+                  : Theme.of(context).primaryColor,
+            ),
+            child: Text(
+              'About',
+              style: TextStyle(
+                fontStyle: FontStyle.normal,
+                color: themeProvider.darkTheme
+                    ? Theme.of(context).buttonTheme.colorScheme.surface
+                    : Theme.of(context).primaryColor,
+              ),
+            ),
+             applicationName: 'Trivia',
+              applicationVersion: '1.0.0',
+              applicationIcon: Container(
+                padding: const EdgeInsets.all(8.0),
+                width: 50,
+                height: 50,
+                decoration: BoxDecoration(
+                   borderRadius: BorderRadiusDirectional.circular(12),
+                  color: Theme.of(context).primaryColor,
+                ),
+                child: Image.asset('assets/images/logo.png'),
+              ),
+              aboutBoxChildren: [
+                Text('A trivia application to have fun, if you like!')
+              ]
+          ),
+          
         ),
       ],
     );
