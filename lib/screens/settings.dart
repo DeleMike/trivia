236a753d3b448da1271e2f95100c46d558cf3ef6
DeleMike <akindelemichael65@gmail.com>
2021-08-13 14:13:47 +0100

    add app default theme option

--- lib/screens/settings.dart
+++ lib/screens/settings.dart
@@ -14,72 +14,87 @@ class Settings extends StatefulWidget {
 }
 
 class _SettingsState extends State<Settings> {
-  var _value = false;
+  bool _isDarkMode = false;
+  bool _isAppInDarkMode = false;
+  bool _isAppDefaultThemeActive = true;
 
   @override
   Widget build(BuildContext context) {
-    final themeChange = Provider.of<DarkThemeProvider>(context);
+    // final themeChange = Provider.of<DarkThemeProvider>(context);
     return Scaffold(
       appBar: AppBar(title: Text('Settings')),
       body: Container(
         padding: const EdgeInsets.only(top: 1.0),
-        child: SettingsList(
-          sections: [
-            SettingsSection(
-              title: 'Preferences',
-              titleTextStyle: TextStyle(
-                  color: themeChange.darkTheme ? Colors.white : Colors.black),
-              tiles: [
-                SettingsTile.switchTile(
-                  title: 'Dark Mode',
-                  leading: Icon(
-                    _value ? Icons.bedtime : Icons.bedtime_outlined,
-                    color: Colors.indigo[600],
+        child: Consumer<DarkThemeProvider>(
+          builder: (_, theme, __) => SettingsList(
+            sections: [
+              SettingsSection(
+                title: 'Preferences',
+                titleTextStyle: TextStyle(
+                    color: theme.darkTheme ? Colors.white : Colors.black),
+                tiles: [
+                  SettingsTile.switchTile(
+                    title: 'Dark Mode',
+                    leading: Icon(
+                      _isDarkMode ? Icons.bedtime : Icons.bedtime_outlined,
+                      color: Colors.indigo[600],
+                    ),
+                    enabled: !theme.isAppDefaultThemeActive,
+                    switchValue: theme.darkTheme,
+                    switchActiveColor: Colors.indigo[600],
+                    onToggle: (bool val) {
+                      theme.darkTheme = val;
+                      print('Settings: Dark Mode = $val');
+                    },
                   ),
-                  switchValue: themeChange.darkTheme,
-                  switchActiveColor: Colors.indigo[600],
-                  onToggle: (bool val) {
-                    themeChange.darkTheme = val;
-                    print('Settings: Dark Mode = $val');
-                  },
-                ),
-                SettingsTile(
-                  title: 'Update bio',
-                  subtitle: 'Change your name and profile pic',
-                  leading: Icon(
-                    themeChange.darkTheme
-                        ? Icons.person
-                        : Icons.person_outlined,
-                    color: Colors.indigo[600],
+                  SettingsTile(
+                    title: 'Update bio',
+                    subtitle: 'Change your name and profile pic',
+                    leading: Icon(
+                      theme.darkTheme ? Icons.person : Icons.person_outlined,
+                      color: Colors.indigo[600],
+                    ),
+                    onPressed: (ctx) {
+                      Navigator.of(context).pushNamed(AuthScreen.routeName);
+                    },
                   ),
-                  onPressed: (ctx) {
-                    Navigator.of(context).pushNamed(AuthScreen.routeName);
-                  },
-                ),
-                SettingsTile(
-                  title: 'Check device theme',
-                  subtitle: 'Get system default current theme',
-                  leading: Icon(
-                    themeChange.darkTheme
-                        ? Icons.circle
-                        : Icons.circle_outlined,
-                    color: Colors.indigo[600],
+                  SettingsTile.switchTile(
+                    title: 'System default theme',
+                    subtitle: 'Apply your phone\'s current theme to this app',
+                    leading: Icon(
+                      _isDarkMode ? Icons.circle : Icons.circle_outlined,
+                      color: Colors.indigo[600],
+                    ),
+                    switchValue: theme.isAppDefaultThemeActive,
+                    switchActiveColor: Colors.indigo[600],
+                    onToggle: (_) {
+                      //get app default settings
+                      var brightness =
+                          MediaQuery.of(context).platformBrightness;
+                      _isAppInDarkMode = brightness == Brightness.dark;
+
+                      //update saved settings
+                      //if true, use app's theme and make the `Dark Mode` inactive
+                      //if false, use `Dark Mode` options.
+                      theme.isAppDefaultThemeActive = _isAppInDarkMode;
+
+                      // print(
+                      //     'Settings: App is in dark Mode = $_isAppInDarkMode');
+
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text(
+                            'Settings: App is in dark Mode = $_isAppInDarkMode',
+                          ),
+                          duration: Duration(seconds: 2),
+                        ),
+                      );
+                    },
                   ),
-                  onPressed: (ctx) {
-                    var brightness = MediaQuery.of(ctx).platformBrightness;
-                    print('Current Brightness of device is $brightness');
-                    ScaffoldMessenger.of(ctx).showSnackBar(
-                      SnackBar(
-                        content:
-                            Text('Current Brightness of device is $brightness'),
-                        duration: Duration(seconds: 2),
-                      ),
-                    );
-                  },
-                ),
-              ],
-            ),
-          ],
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
