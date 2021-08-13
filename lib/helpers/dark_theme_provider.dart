d0057d087ab54c8ebdccc5f8e09d9db9418052b4
DeleMike <akindelemichael65@gmail.com>
2021-08-13 14:13:24 +0100

    add app default theme

--- lib/helpers/dark_theme_provider.dart
+++ lib/helpers/dark_theme_provider.dart
@@ -6,11 +6,15 @@ import '../helpers/dark_theme_pref.dart';
 class DarkThemeProvider with ChangeNotifier {
   DarkThemePreference darkThemePreference = DarkThemePreference();
   bool _darkTheme = false;
+  bool _isAppDefaultThemeActive = false;
 
   ///get if dark theme should be applied
   bool get darkTheme => _darkTheme;
 
-  ///set the desired theme type by returning 
+  ///get value if app theme has been selected on the app
+  bool get isAppDefaultThemeActive => _isAppDefaultThemeActive;
+
+  ///set the desired theme type by returning
   ///```true``` for dark theme or ```false``` for light theme
   set darkTheme(bool value) {
     _darkTheme = value;
@@ -18,4 +22,9 @@ class DarkThemeProvider with ChangeNotifier {
     notifyListeners();
   }
 
+  ///set value for ```isAppDefaultThemeActive```
+  set isAppDefaultThemeActive(bool val) {
+    _isAppDefaultThemeActive = val;
+    notifyListeners();
+  }
 }
